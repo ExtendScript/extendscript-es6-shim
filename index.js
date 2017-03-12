830a291ba5a88e0aed7774dc8fa6987c961a5614
fabianmoronzirfas <icke@fabianmoronzirfas.me>
2017-03-12 07:54:25 +0100

    initial local commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//.gitkeep
+
+//.gitkeep
+
+//.gitkeep
+
+//assign.js
+/**
+ * https://gist.github.com/WebReflection/10404826
+ */
+/* eslint no-use-before-define: off */
+
+try {
+  Object.assign({}, {foo: 'bar'});
+}catch(err) {
+  // failed: so we're in IE8
+  (function() {
+    Object.assign = (function(has) {
+      'use strict';
+      return assign;
+      function assign(target, source) {
+        for (var i = 1; i < arguments.length; i++) {
+          copy(target, arguments[i]);
+        }
+        return target;
+      }
+      function copy(target, source) {
+        for (var key in source) {
+          if (has.call(source, key)) {
+            target[key] = source[key];
+          }
+        }
+      }
+    }({}.hasOwnProperty));
+  }());
+}
+
