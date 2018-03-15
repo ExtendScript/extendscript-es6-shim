345a4b7a40895fee66580038b24cb1a6ee2202df
fabianmoronzirfas <fabian.moron.zirfas@gmail.com>
2018-03-15 20:47:55 +0100

    modified:   bin/concat.js modified:   index.js new file:   package-lock.json modified:   package.json new file:   test/array.from.test.jsx

--- bin/concat.js
+++ bin/concat.js
@@ -1,19 +1,18 @@
 const folders = ['./Array/', './Function/', './Object/', './String/'];
-// const testFolder = './Array/';
 const fs = require('fs');
 const path = require('path');
 const cat = require('cat');
 const bundlePath = path.resolve(process.cwd(), './index.js');
-
 // clear the file
 fs.writeFileSync(path.resolve(process.cwd(), bundlePath), '');
 // loop folders
 folders.forEach(folder => {
   fs.readdir(folder, (err, files) => {
     files.forEach(file => {
-      // console.log(file);
       let filePath = path.resolve(process.cwd(), `${folder}${file}`);
-      // console.log(filePath);
+      if(file === '.gitkeep') {
+        return;
+      }
       cat(filePath, (error, data)=>{
         if(error !== null) {
           console.log(error);
@@ -23,5 +22,4 @@ folders.forEach(folder => {
       });
     });
   });
-
 });
---
+++ test/array.from.test.jsx
@@ -0,0 +1,8 @@
+﻿#include "../Array/from.js"
+$.writeln(Array.from("foo"));
+// => f,o,o
+#target "indesign"
+var doc = app.documents.add();
+doc.pages.add();
+$.writeln(Array.from());
+// => [object Page], [object Page]
